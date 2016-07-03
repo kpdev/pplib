d61bbe7e13c6faf792071e0707d30df75e11c2f1
kpdev <kpdevelop@mail.ru>
2016-07-04 01:44:30 +0300

    Remove unnecessary include

--- pplib/CreateMM.h
+++ pplib/CreateMM.h
@@ -1,4 +1,4 @@
-#include <memory>
+//#include <memory>
 
 #define DECLARE_MM(TypeName, Dimention, BaseType, ...) \
 typedef void (*TypeName)(BaseType&, BaseType&, __VA_ARGS__); \
@@ -7,10 +7,13 @@ extern TypeName TypeName##MMArray[][Dimention]; \
 template<typename ...ArgsT> \
 void TypeName##MM(BaseType &p1, BaseType &p2, ArgsT ...args) \
 { \
-	(TypeName##MMArray[p1.mark][p2.mark])( p1, p2, std::forward<ArgsT>(args)... ); \
+	(TypeName##MMArray[p1.mark][p2.mark])( p1, p2, args... ); \
 }
 
 #define DEFINE_MM(TypeName, Dimention) \
 TypeName TypeName##MM##Array[Dimention][Dimention]; \
  
-
+// If R-Value support will be needed, then this should be applied:
+//	#include <memory>
+//	...
+//	(TypeName##MMArray[p1.mark][p2.mark])( p1, p2, std::forward<Args>(args)... );
